b840b3151b26487057f9c137b7a8866926317a25
lukivan8 <lukivan888@gmail.com>
2024-10-04 14:29:39 +0500

    finish pocketbase setup

---
+++ src/api/pocketbase.ts
@@ -0,0 +1,10 @@
+import PocketBase from "pocketbase";
+import { TypedPocketBase } from "./api_types";
+
+export type Sorting = "+created" | "-created";
+
+const pocketbase = new PocketBase(
+  "https://higher-base.pockethost.io"
+) as TypedPocketBase;
+
+export default pocketbase;
