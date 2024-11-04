c06f6176d3d1c71aa4eb0fa8f1f593d008e3ce1c
lukivan8 <lukivan888@gmail.com>
2024-11-04 19:29:10 +0500

    fix pocketbase

--- src/api/client_pb.ts
+++ src/api/client_pb.ts
@@ -1,8 +1,7 @@
 import PocketBase from "pocketbase";
 import { TypedPocketBase } from "./api_types";
+import { pocketbaseHost } from "./pocketbase";
 
-const clientPocketBase = new PocketBase(
-  "https://pocketbase.nextmove.kz"
-) as TypedPocketBase;
+const clientPocketBase = new PocketBase(pocketbaseHost) as TypedPocketBase;
 clientPocketBase.autoCancellation(false);
 export default clientPocketBase;
